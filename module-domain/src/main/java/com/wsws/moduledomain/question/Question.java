f3568221283a4ce3c1ad0e8289d483cf016761dd
jhk01007 <jhk001007@naver.com>
2024-11-26 22:47:45 +0900

    [QFEED-50] Feat: Question 엔티티 클래스 추가

---
+++ module-domain/src/main/java/com/wsws/moduledomain/question/Question.java
@@ -0,0 +1,21 @@
+package com.wsws.moduledomain.question;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class Question {
+    @Id @GeneratedValue
+    Long id;
+
+    private String content;
+    private boolean isToday;
+    private LocalDateTime createdAt;
+
+
+}
