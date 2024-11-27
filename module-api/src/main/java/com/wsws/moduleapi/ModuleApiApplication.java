20be4f7bb8f5f9e5f96a2eca69dcddd059b71846
LeeHyeonseob <hyeonseob22@gmail.com>
2024-11-27 16:55:26 +0900

    [QFEED-39] Feature: 스캔할 패키지 추가

--- module-api/src/main/java/com/wsws/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/com/wsws/moduleapi/ModuleApiApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
-		"com.wsws.moduleapi",
 		"com.wsws.moduleapplication",
-		"com.wsws.moduledomain"
+		"com.wsws.moduledomain",
+		"com.wsws.moduleinfra",
+		"com.wsws.modulecommon",
+		"com.wsws.modulesecurity"
 })
 public class ModuleApiApplication {
 	public static void main(String[] args) {
