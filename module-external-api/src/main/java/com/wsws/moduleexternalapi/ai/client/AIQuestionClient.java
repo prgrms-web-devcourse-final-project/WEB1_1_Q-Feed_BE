c21826b757e930645ef7e46de9ab35791c3c4ebf
jhk01007 <jhk001007@naver.com>
2024-11-27 22:36:28 +0900

    [QFEED-50] Feat: OpenAI 연동 및 질문 생성 기능 구현
    - AIQuestionResponse 형식으로 각 카테고리의 질문을 받아옴
    - Json 형식으로 질문을 받아오도록 설정함
    - 추후 LLM 모델 변경시 유지보수의 용이성을 위해 질문 생성 책임을 가지는 클래스에는 특정 LLM 모델 관련 클래스 의존하지 않도록 설계
    - temperature를 1.2로 높혀 질문이 다양하게 출력되게 끔 하였음.

---
+++ module-external-api/src/main/java/com/wsws/moduleexternalapi/ai/client/AIQuestionClient.java
@@ -0,0 +1,62 @@
+package com.wsws.moduleexternalapi.ai.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Component
+public class AIQuestionClient {
+    private final ChatModel chatModel;
+    private final ChatOptions chatOptions;
+
+    private static final String SYSTEM_PROMPT = """ 
+            You are an expert assistant that creates engaging and relevant content for the MZ generation in their 20s and 30s in Korea. Your task is to provide natural and appealing responses.
+            """;
+    private static final String USER_PROMPT = """ 
+            Your task is to create one engaging and relevant question for each of the {categoriesSize} categories: {categories}
+            The target audience is the MZ generation in their 20s and 30s in Korea. 
+            Provide the questions in Korean and ensure the response is a 'pure JSON object without any Markdown formatting(do not include ```json)'. Ensure the questions are natural and appealing.
+            """;
+
+    /**
+     * 생성해야하는 카테고리 목록들을 받아 프롬프트를 작성해 질문 생성
+     */
+    public Map<String, String> createQuestions(List<String> categories) {
+        Prompt prompt = createPrompt(categories);
+        System.out.println(prompt);
+        ChatResponse response = chatModel.call(prompt);
+        System.out.println(response);
+        String content = response.getResult().getOutput().getContent();
+        System.out.println(content);
+        return null;
+    }
+
+
+    /**
+     *
+     * @param categories
+     * @return
+     */
+    private Prompt createPrompt(List<String> categories) {
+
+        // 시스템 프롬프트 생성
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(SYSTEM_PROMPT);
+        Message systemMessage = systemPromptTemplate.createMessage();
+
+        // 사용자 프롬프트 생성
+        PromptTemplate userPromptTemplate = new PromptTemplate(USER_PROMPT);
+        Message userMessage = userPromptTemplate.createMessage(Map.of("categoriesSize", categories.size(), "categories", categories.toString()));
+
+        return new Prompt(List.of(systemMessage, userMessage), chatOptions);
+    }
+}
---
+++ module-external-api/src/main/java/com/wsws/moduleexternalapi/ai/dto/AIQuestionResponse.java
@@ -0,0 +1,12 @@
+package com.wsws.moduleexternalapi.ai.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+public record AIQuestionResponse(
+        @JsonProperty(required = true, value = "travel") String travel,
+        @JsonProperty(required = true, value = "deliciousRestaurant") String deliciousRestaurant,
+        @JsonProperty(required = true, value = "movie") String movie,
+        @JsonProperty(required = true, value = "music") String music,
+        @JsonProperty(required = true, value = "reading") String reading,
+        @JsonProperty(required = true, value = "sports") String sports) {
+}
+
