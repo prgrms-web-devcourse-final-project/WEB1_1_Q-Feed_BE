de4deb8190a16b69e784828cf7fc0685671412eb
jhk01007 <jhk001007@naver.com>
2024-11-27 10:45:02 +0900

    [QFEED-55] Feat: Common Layer: 에러코드 Enum 추가
    - BaseErrorCode: 에러코드 인터페이스
    - GlobalErrorCode: 전역적인 에러코드
    - ErrorInfo: 예외정보

---
+++ module-common/src/main/java/com/wsws/moduleapi/exception/ErrorInfo.java
@@ -0,0 +1,7 @@
+package com.wsws.moduleapi.exception;
+
+public record ErrorInfo(Integer status, String errorCode, String message) {
+    public static ErrorInfo of(Integer status, String errorCode, String message) {
+        return new ErrorInfo(status, errorCode, message);
+    }
+}
