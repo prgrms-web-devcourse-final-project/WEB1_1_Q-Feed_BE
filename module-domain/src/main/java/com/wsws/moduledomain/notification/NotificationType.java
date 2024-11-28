1621bc10a93e1a82b587e66c6715a47313cc7a44
김지은 <wldms5119@naver.com>
2024-11-28 10:08:54 +0900

    Feature: Notification 도메인 구현

---
+++ module-domain/src/main/java/com/wsws/moduledomain/notification/NotificationType.java
@@ -0,0 +1,5 @@
+package com.wsws.moduledomain.notification;
+
+public enum NotificationType{
+    FOLLOW, COMMENT, CHAT, LIKE, Q_SPACE_COMMENT, Q_SPACE_LIKE
+}
---
+++ module-domain/src/main/java/com/wsws/moduledomain/notification/vo/NotificationContent.java
@@ -0,0 +1,25 @@
+package com.wsws.moduledomain.notification.vo;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NotificationContent {
+    private String content;
+
+    public NotificationContent(String content) {
+        if (content == null || content.isBlank()){
+            throw new IllegalArgumentException("알림 내용은 비어 있습니다.");
+        }
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
