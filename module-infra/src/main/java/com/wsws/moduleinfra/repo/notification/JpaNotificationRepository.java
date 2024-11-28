034fcda1e633429abb7ff819a4b736593ac758b4
김지은 <wldms5119@naver.com>
2024-11-28 21:52:15 +0900

    Feature: notification infra 구현
     - JpaNotificationRepository 구현

---
+++ module-infra/src/main/java/com/wsws/moduleinfra/repo/notification/JpaNotificationRepository.java
@@ -0,0 +1,20 @@
+package com.wsws.moduleinfra.repo.notification;
+
+import com.wsws.moduledomain.notification.Notification;
+import com.wsws.moduledomain.notification.repo.NotificationRepository;
+import com.wsws.moduledomain.user.vo.UserId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JpaNotificationRepository extends JpaRepository<Notification, Long>, NotificationRepository {
+
+    @Override
+    List<Notification> findByRecipientIdAndIsReadFalse(UserId recipientId);
+
+    @Override
+    List<Notification> findByRecipientId(UserId recipientId);
+}
+
