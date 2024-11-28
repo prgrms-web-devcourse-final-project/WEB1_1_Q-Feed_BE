d9b2542f0b79781f14fc3ce4e228c0710a15ac8c
LeeHyeonseob <hyeonseob22@gmail.com>
2024-11-28 11:24:25 +0900

    [QFEED-39] Chore: EntityScan package 설정

--- module-infra/src/main/java/com/wsws/moduleinfra/config/JpaConfig.java
+++ module-infra/src/main/java/com/wsws/moduleinfra/config/JpaConfig.java
@@ -1,9 +1,11 @@
-package com.wsws.moduleapplication.config;
+package com.wsws.moduleinfra.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.wsws.moduleinfra.repo") // infra 모듈의 JPA Repository 경로
+@EntityScan(basePackages = "com.wsws.moduledomain")
 public class JpaConfig {
 }
--- module-infra/src/main/java/com/wsws/moduleinfra/config/RedisConfig.java
+++ module-infra/src/main/java/com/wsws/moduleinfra/config/RedisConfig.java
@@ -1,18 +1,26 @@
 package com.wsws.moduleinfra.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private int port;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379); // 이거는
+        return new LettuceConnectionFactory(host, port); // 이거는
     }
 
     @Bean
@@ -21,6 +29,15 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer()); // Key 직렬화 설정
         redisTemplate.setValueSerializer(new StringRedisSerializer()); // Value 직렬화 설정
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, Integer> redisTemplateInteger(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
     }
 }
