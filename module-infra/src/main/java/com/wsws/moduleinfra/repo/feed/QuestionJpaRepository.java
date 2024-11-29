010bcfaf5cdff93b201ab5e555a38850d02e2c58
jhk01007 <jhk001007@naver.com>
2024-11-29 09:25:33 +0900

    [QFEED-50] Rename: QuestionJpaRepository 디렉토리 구조 변경

--- module-infra/src/main/java/com/wsws/moduleinfra/repo/feed/QuestionJpaRepository.java
+++ module-infra/src/main/java/com/wsws/moduleinfra/repo/feed/QuestionJpaRepository.java
@@ -1,13 +1,11 @@
-package com.wsws.moduleinfra.feed.repo;
+package com.wsws.moduleinfra.repo.feed;
 
 import com.wsws.moduledomain.feed.question.Question;
 import com.wsws.moduledomain.feed.question.repo.QuestionRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
-public interface QuestionJpaRepository extends QuestionRepository, JpaRepository<Question, Long> {
+public interface QuestionJpaRepository extends JpaRepository<Question, Long>, QuestionRepository{
 
 }
