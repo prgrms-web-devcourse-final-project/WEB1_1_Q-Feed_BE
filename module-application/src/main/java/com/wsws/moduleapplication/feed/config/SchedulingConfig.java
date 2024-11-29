ad7636db707031f5947353390686f090a3e14470
jhk01007 <jhk001007@naver.com>
2024-11-29 12:20:48 +0900

    [QFEED-50] Feat: 질문 상태 업데이트 스케줄링 로직 추가

---
+++ module-application/src/main/java/com/wsws/moduleapplication/feed/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.wsws.moduleapplication.feed.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
