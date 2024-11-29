8e1a248ec3f3b793be82ece5c67289245f81bd62
김지은 <wldms5119@naver.com>
2024-11-29 11:11:39 +0900

    Feature: api 계층 알림 관련 dto 추가

---
+++ module-api/src/main/java/com/wsws/moduleapi/dto/notification/NotificationRequestDto.java
@@ -0,0 +1,19 @@
+package com.wsws.moduleapi.dto.notification;
+
+import com.wsws.moduledomain.notification.NotificationType;
+import com.wsws.moduledomain.user.vo.UserId;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record NotificationRequestDto(
+        @NotBlank
+        UserId recipientId,
+        @NotNull
+        NotificationType type,
+        @NotBlank
+        String content,
+        @NotBlank
+        UserId senderId
+        //fcm 토큰
+) {
+}
