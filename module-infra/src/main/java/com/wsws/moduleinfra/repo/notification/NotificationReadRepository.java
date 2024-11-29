45e0d021337dd9b86320e2ea6c7e1f890ba961c2
김지은 <wldms5119@naver.com>
2024-11-29 11:05:00 +0900

    Feature: 알림 조회를 위한 Repository 인터페이스 구현체 추가

---
+++ module-infra/src/main/java/com/wsws/moduleinfra/repo/notification/NotificationReadRepository.java
@@ -0,0 +1,13 @@
+package com.wsws.moduleinfra.repo.notification;
+
+import com.wsws.moduledomain.notification.Notification;
+import com.wsws.moduledomain.user.vo.UserId;
+import com.wsws.moduleinfra.repo.notification.dto.NotificationResponseInfraDto;
+
+import java.util.List;
+
+public interface NotificationReadRepository {
+
+    // 알림 조회
+    List<NotificationResponseInfraDto> findByRecipientIdAndIsReadFalse(UserId recipientId);
+}
