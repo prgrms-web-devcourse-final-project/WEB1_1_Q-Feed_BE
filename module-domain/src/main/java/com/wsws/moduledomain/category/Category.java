1792b9c86485192e9720180524c864077d193b70
jhk01007 <jhk001007@naver.com>
2024-11-30 16:19:56 +0900

    [QFEED-52] Feat: Category 엔티티에 @Getter 추가

--- module-domain/src/main/java/com/wsws/moduledomain/category/Category.java
+++ module-domain/src/main/java/com/wsws/moduledomain/category/Category.java
@@ -2,8 +2,10 @@ package com.wsws.moduledomain.category;
 
 import com.wsws.moduledomain.category.vo.CategoryName;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Category {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
