30bed97e3c9a3ee749b700ca529fa38525c55adb
jhk01007 <jhk001007@naver.com>
2024-11-30 13:52:51 +0900

    [QFEED-52] Feat: Redis Vector Database 빈 수동 등록

---
+++ module-external-api/src/main/java/com/wsws/moduleexternalapi/feed/config/RedisVectorStoreConfig.java
@@ -0,0 +1,45 @@
+package com.wsws.moduleexternalapi.feed.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.RedisVectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPooled;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisVectorStoreConfig {
+
+    @Value("${spring.ai.vectorstore.redis.uri}")
+    private String redisHost;
+
+    @Value("${spring.ai.vectorstore.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.ai.vectorstore.redis.index}")
+    private String redisIndex;
+
+    @Value("${spring.ai.vectorstore.redis.prefix}")
+    private String redisPredix;
+
+
+
+    @Bean
+    public JedisPooled jedisPooled() {
+        // Redis 서버의 호스트와 포트를 실제 환경에 맞게 설정하세요.
+        return new JedisPooled(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisVectorStore redisVectorStore(JedisPooled jedisPooled, EmbeddingModel embeddingModel) {
+        RedisVectorStore.RedisVectorStoreConfig config = RedisVectorStore.RedisVectorStoreConfig.builder()
+                .withIndexName(redisIndex)
+                .withPrefix(redisPredix)
+                .build();
+
+        return new RedisVectorStore(config, embeddingModel, jedisPooled, true);
+    }
+}
