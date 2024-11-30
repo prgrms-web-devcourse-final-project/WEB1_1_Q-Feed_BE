730a0c2e5e2b30c5bd5b70dcb257f282de942968
jhk01007 <jhk001007@naver.com>
2024-11-30 16:16:26 +0900

    [QFEED-52] Refactor: 생성된 질문 형식 내에 변수명 대문자로 변경
    - CategoryName Enum과 동일하게 하였음

--- module-external-api/src/main/java/com/wsws/moduleexternalapi/feed/dto/AIQuestionResponse.java
+++ module-external-api/src/main/java/com/wsws/moduleexternalapi/feed/dto/AIQuestionResponse.java
@@ -2,11 +2,11 @@ package com.wsws.moduleexternalapi.feed.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 public record AIQuestionResponse(
-        @JsonProperty(required = true, value = "travel") String travel,
-        @JsonProperty(required = true, value = "deliciousRestaurant") String deliciousRestaurant,
-        @JsonProperty(required = true, value = "movie") String movie,
-        @JsonProperty(required = true, value = "music") String music,
-        @JsonProperty(required = true, value = "reading") String reading,
-        @JsonProperty(required = true, value = "sports") String sports) {
+        @JsonProperty(required = true, value = "TRAVEL") String travel,
+        @JsonProperty(required = true, value = "DELICIOUS_RESTAURANT") String deliciousRestaurant,
+        @JsonProperty(required = true, value = "MOVIE") String movie,
+        @JsonProperty(required = true, value = "MUSIC") String music,
+        @JsonProperty(required = true, value = "READING") String reading,
+        @JsonProperty(required = true, value = "SPORTS") String sports) {
 }
 
