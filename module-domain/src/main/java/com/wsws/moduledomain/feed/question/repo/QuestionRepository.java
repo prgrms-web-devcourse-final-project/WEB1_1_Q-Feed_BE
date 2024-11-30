604686b5cbc2e96a8f1d6f5f774091fd6e5e36d3
jhk01007 <jhk001007@naver.com>
2024-11-30 16:09:22 +0900

    [QFEED-52] Feat: QuestionRepository에 Question 저장 메서드 추가

--- module-domain/src/main/java/com/wsws/moduledomain/feed/question/repo/QuestionRepository.java
+++ module-domain/src/main/java/com/wsws/moduledomain/feed/question/repo/QuestionRepository.java
@@ -10,4 +10,6 @@ public interface QuestionRepository {
     // 질문 상태를 기준으로 질문 가져오기
     List<Question> findByQuestionStatus(QuestionStatus questionStatus);
 
+    // 질문 저장
+    Question save(Question question);
 }
