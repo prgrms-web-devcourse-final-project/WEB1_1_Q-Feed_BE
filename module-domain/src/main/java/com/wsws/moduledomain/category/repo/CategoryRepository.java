57b5d472713bfe567d99bc2edf8906d9e38f094c
jhk01007 <jhk001007@naver.com>
2024-12-01 18:22:11 +0900

    [QFEED-52] Feat: CategoryRepository 저장 메서드 추가

--- module-domain/src/main/java/com/wsws/moduledomain/category/repo/CategoryRepository.java
+++ module-domain/src/main/java/com/wsws/moduledomain/category/repo/CategoryRepository.java
@@ -8,4 +8,9 @@ public interface CategoryRepository {
      * 카테고리 이름으로 카테고리 받아오기
      */
     Category findByCategoryName(CategoryName categoryName);
+
+    /**
+     * 카테고리 저장하기
+     */
+    Category save(Category category);
 }
