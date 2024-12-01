db5a8ad6a11cbddf35ca5a6f1b8a6874c20b3a84
jhk01007 <jhk001007@naver.com>
2024-12-01 17:04:08 +0900

    [QFEED-52] Refactor: 질문 저장 시점을 모든 카테고리 질문들의 중복 검증이 끝났을 때로 변경
    - 질문 생성 도중 오류 발생시 데이터 불일치를 방지.

--- module-application/src/main/java/com/wsws/moduleapplication/feed/service/scheduler/ScheduledQuestionCreateService.java
+++ module-application/src/main/java/com/wsws/moduleapplication/feed/service/scheduler/ScheduledQuestionCreateService.java
@@ -15,10 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.wsws.moduledomain.category.vo.CategoryName.*;
 import static com.wsws.moduleexternalapi.feed.util.TokenCalculateUtil.*;
@@ -36,6 +33,7 @@ public class ScheduledQuestionCreateService {
 
     private List<String> categories;
     private Map<String, List<String>> questionBlackList;
+    private Map<String, String> questionTempStore; // 검증이 끝난 질문 임시 저장소
 
     /**
      * 매일 23시 30분에 질문을 생성
@@ -45,7 +43,7 @@ public class ScheduledQuestionCreateService {
         initList(); // 리스트 초기화
 
         // 모든 카테고리의 질문들이 생성될 때 까지 반복
-        while(!categories.isEmpty()) {
+        while (!categories.isEmpty()) {
             Map<String, String> createdQuestions = questionGenerateClient.createQuestions(categories, questionBlackList);
 
             // 질문 검증 및 저장
@@ -54,11 +52,13 @@ public class ScheduledQuestionCreateService {
                 List<String> similarQuestions = redisVectorClient.findSimilarText(question);
 
                 if (similarQuestions.isEmpty()) { // 중복되지 않는 질문일 때,
-                    saveQuestion(categoryName, question); // 데이터베이스에 질문 저장
+                    log.info("질문 검증 완료: {}: {}", categoryName, question);
+                    questionTempStore.put(categoryName, question); // 질문 리스트에 임시 저장
+                    removeCategoryFromList(categoryName); // 저장한 질문의 카테고리는 리스트에서 제외
                 } else { // 중복된 질문이면
                     // 질문 블랙 리스트에 추가
                     log.info("질문 중복: {}: {}", categoryName, question);
-                    if(questionBlackList.containsKey(categoryName)) { // 생성된 질문 블랙리스트에 저장
+                    if (questionBlackList.containsKey(categoryName)) { // 생성된 질문 블랙리스트에 저장
                         questionBlackList.get(categoryName).add(question);
                     } else {
                         questionBlackList.put(categoryName, new ArrayList<>(List.of(question)));
@@ -67,7 +67,9 @@ public class ScheduledQuestionCreateService {
                 }
             }
         }
-        log.info("모든 질문 생성완료. 사용된 누적 토큰 수: [입력토큰: {}, 출력토큰: {}, 총합: {}", getPromptToken(), getGenerationToken(), getTotalToken());
+        log.info("모든 질문 생성완료.");
+        saveQuestion(); // 데이터베이스에 질문 저장.
+        log.info(" 사용된 누적 토큰 수: [입력토큰: {}, 출력토큰: {}, 총합: {}]", getPromptToken(), getGenerationToken(), getTotalToken());
     }
 
 
@@ -77,23 +79,31 @@ public class ScheduledQuestionCreateService {
     private void initList() {
         categories = new ArrayList<>(List.of(TRAVEL.name(), DELICIOUS_RESTAURANT.name(), MOVIE.name(), MUSIC.name(), READING.name(), SPORTS.name()));
         questionBlackList = new HashMap<>();
+        questionTempStore = new HashMap<>();
     }
 
     /**
      * 데이터베이스에 질문 저장
+     * 질문 저장은 모든 질문들의 검증이 끝난 후에 이루어진다.
+     * -> 저장해야하는 데이터베이스가 두 개이기 때문에 데이터 불일치를 방지하기 위함.
      */
-    private void saveQuestion(String categoryName, String question) {
-        redisVectorClient.store(question); // 벡터 데이터베이스에 질문 저장
-        Category category = categoryRepository.findByCategoryName(CategoryName.valueOf(categoryName));
-        questionRepository.save(Question.create(question, QuestionStatus.CREATED, LocalDateTime.now(), category.getId())); // Question 엔티티 RDBMS에 저장
+    private void saveQuestion() {
+        log.info("데이터베이스에 질문을 저장");
 
-        removeList(categoryName); // 저장한 질문의 카테고리는 리스트에서 제외
+        // 벡터 데이터베이스에 질문 저장
+        redisVectorClient.store(questionTempStore.values().stream().toList());
+
+        // RDBMS에 질문 저장
+        for (String c : questionTempStore.keySet()) {
+            Category category = categoryRepository.findByCategoryName(CategoryName.valueOf(c)); // 카테고리에 ID를 받아옴.
+            questionRepository.save(Question.create(questionTempStore.get(c), QuestionStatus.CREATED, LocalDateTime.now(), category.getId())); // Question 엔티티 RDBMS에 저장
+        }
     }
 
     /**
      * 해당 카테고리 리스트에서 제외
      */
-    private void removeList(String categoryName) {
+    private void removeCategoryFromList(String categoryName) {
         categories.remove(categoryName);
         questionBlackList.remove(categoryName);
     }
