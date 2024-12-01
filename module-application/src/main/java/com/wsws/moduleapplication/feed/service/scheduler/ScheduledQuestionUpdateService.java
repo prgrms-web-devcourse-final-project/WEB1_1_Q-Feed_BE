bd6fa27a41063f6328c7d5ceaa8abf1dfd7011d4
jhk01007 <jhk001007@naver.com>
2024-12-02 01:32:19 +0900

    [QFEED-52] Feat: 스케줄링 실패시 재시도 기능 및 재시도 제한 기능 추가

--- module-application/src/main/java/com/wsws/moduleapplication/feed/service/scheduler/ScheduledQuestionUpdateService.java
+++ module-application/src/main/java/com/wsws/moduleapplication/feed/service/scheduler/ScheduledQuestionUpdateService.java
@@ -1,15 +1,10 @@
 package com.wsws.moduleapplication.feed.service.scheduler;
 
 import com.wsws.moduleapplication.feed.service.QuestionAIService;
-import com.wsws.moduledomain.feed.question.Question;
-import com.wsws.moduledomain.feed.question.repo.QuestionRepository;
-import com.wsws.moduledomain.feed.question.vo.QuestionStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,8 +19,29 @@ public class ScheduledQuestionUpdateService {
      */
     @Scheduled(cron = "0 0 0 * * ?") // 매일 00시 00분에 실행되도록 설정
     public void updateQuestions() {
+        int maxRetries = 3; // 최대 재시도 횟수
+        int attempt = 0;    // 현재 시도 횟수
 
-        questionAIService.updateQuestions();
-        log.info("스케줄링 작업 완료");
+        while (attempt < maxRetries) {
+            try {
+                attempt++;
+                log.info("스케줄링 작업 실행, 시도 횟수: {}", attempt);
+
+                // 질문 상태 업데이트 로직
+                questionAIService.updateQuestions();
+
+                // 성공하면 루프 종료
+                log.info("스케줄링 작업 완료, 시도 횟수: {}", attempt);
+                break;
+            } catch (Exception e) {
+                log.error("스케줄링 작업 실패, 시도 횟수: {}, 에러: {}", attempt, e.getMessage());
+
+                // 마지막 시도에서 실패했을 때 추가 처리
+                if (attempt >= maxRetries) {
+                    log.error("스케줄링 작업 모든 재시도 실패. ");
+                    // TODO: 이메일로 알림
+                }
+            }
+        }
     }
 }
