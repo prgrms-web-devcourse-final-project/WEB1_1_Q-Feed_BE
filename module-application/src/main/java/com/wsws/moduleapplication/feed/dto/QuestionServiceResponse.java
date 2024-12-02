8b47079803c22a870cc0693c7be9db8e9fc73268
jhk01007 <jhk001007@naver.com>
2024-12-02 16:08:11 +0900

    [QFEED-81] Feat: 질문 조회 API 기능 추가

---
+++ module-application/src/main/java/com/wsws/moduleapplication/feed/dto/QuestionServiceResponse.java
@@ -0,0 +1,16 @@
+package com.wsws.moduleapplication.feed.dto;
+
+import com.wsws.moduledomain.feed.question.Question;
+
+public record QuestionServiceResponse(
+        Long questionId,
+        String content
+) {
+
+    public QuestionServiceResponse(Question question) {
+        this(
+                question.getQuestionId().getId(),
+                question.getContent()
+        );
+    }
+}
---
+++ module-application/src/main/java/com/wsws/moduleapplication/feed/service/QuestionService.java
@@ -0,0 +1,25 @@
+package com.wsws.moduleapplication.feed.service;
+
+import com.wsws.moduleapplication.feed.dto.QuestionServiceResponse;
+import com.wsws.moduleapplication.feed.exception.QuestionNotFoundException;
+import com.wsws.moduledomain.feed.question.Question;
+import com.wsws.moduledomain.feed.question.repo.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    /**
+     * 카테고리로 질문 조회
+     */
+    public QuestionServiceResponse findQuestionByCategoryId(Long id) {
+        Question question = questionRepository.findByCategoryId(id).orElseThrow(() -> QuestionNotFoundException.EXCEPTION);
+        return new QuestionServiceResponse(question);
+    }
+
+
+}
