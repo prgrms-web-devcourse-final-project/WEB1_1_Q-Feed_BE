6c32c2f85ad36d1f05246e53ffd500715781ebe6
jhk01007 <jhk001007@naver.com>
2024-12-02 16:08:11 +0900

    [QFEED-81] Feat: 질문 조회 API 기능 추가

--- module-api/src/main/java/com/wsws/moduleapi/feed/dto/question/QuestionApiResponse.java
+++ module-api/src/main/java/com/wsws/moduleapi/feed/dto/question/QuestionApiResponse.java
@@ -1,6 +1,7 @@
 package com.wsws.moduleapi.feed.dto.question;
 
-import com.wsws.moduleapplication.feed.dto.QuestionFindServiceResponse;
+
+import com.wsws.moduleapplication.feed.dto.question.QuestionFindServiceResponse;
 
 public record QuestionApiResponse(
         Long questionId,
