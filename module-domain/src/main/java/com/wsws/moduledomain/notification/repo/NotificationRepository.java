f5c8915b31b11fdcadb53aa00f651abaa235e7ef
김지은 <wldms5119@naver.com>
2024-12-03 06:43:09 +0900

    fix : 도메인 repository 수정

--- module-domain/src/main/java/com/wsws/moduledomain/notification/repo/NotificationRepository.java
+++ module-domain/src/main/java/com/wsws/moduledomain/notification/repo/NotificationRepository.java
@@ -1,15 +1,14 @@
 package com.wsws.moduledomain.notification.repo;
 
 import com.wsws.moduledomain.notification.Notification;
-import com.wsws.moduledomain.user.vo.UserId;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface NotificationRepository {
-    List<Notification> findByRecipientIdAndIsReadFalse(UserId recipientId);
+    List<Notification> findByRecipientIdAndIsReadFalse(String recipientId);
+
     Optional<Notification> findById(Long notificationId);
+
     Notification save(Notification notification);
 }
