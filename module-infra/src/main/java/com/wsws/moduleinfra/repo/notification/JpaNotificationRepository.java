3f39146bdf4d306a6ca58b03b9d9934db5e8b893
김지은 <wldms5119@naver.com>
2024-12-03 06:42:00 +0900

    fix : infra 계층 notification 수정
    - Repository 수정
    
    - infra 응답dto 수정

---
+++ module-infra/src/main/java/com/wsws/moduleinfra/repo/notification/JpaNotificationRepository.java
@@ -0,0 +1,13 @@
+package com.wsws.moduleinfra.repo.notification;
+
+import com.wsws.moduleinfra.NotificationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface JpaNotificationRepository extends JpaRepository<NotificationEntity, Long> {
+    @Query("SELECT n FROM NotificationEntity n WHERE n.recipient.value = :recipientId AND n.isRead = false")
+    List<NotificationEntity> findByRecipient_ValueAndIsReadFalse(@Param("recipientId") String recipientId);
+}
