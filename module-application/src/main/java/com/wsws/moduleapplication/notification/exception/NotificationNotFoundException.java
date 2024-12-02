5e1389bc55fc1b3ebb86bb067211d2bc4fc8ea53
김지은 <wldms5119@naver.com>
2024-12-03 06:31:03 +0900

    fix : 예외처리 수정

---
+++ module-application/src/main/java/com/wsws/moduleapplication/notification/exception/NotificationNotFoundException.java
@@ -0,0 +1,12 @@
+package com.wsws.moduleapplication.notification.exception;
+
+import com.wsws.moduleapi.exception.ApplicationException;
+
+public class NotificationNotFoundException extends ApplicationException {
+    public static final NotificationNotFoundException EXCEPTION = new NotificationNotFoundException();
+
+    private NotificationNotFoundException() {
+
+        super(NotificationServiceErrorCode.NOTIFICATION_NOT_FOUND);
+    }
+}
