3b15268aa18b4784a0a2035d302ddb7f6a627253
jhk01007 <jhk001007@naver.com>
2024-12-03 00:04:43 +0900

    [QFEED-81] Feat: Answer Domain 계층 추가

--- module-domain/src/main/java/com/wsws/moduledomain/feed/answer/repo/AnswerRepository.java
+++ module-domain/src/main/java/com/wsws/moduledomain/feed/answer/repo/AnswerRepository.java
@@ -10,10 +10,20 @@ public interface AnswerRepository {
     /**
      * 답변을 Id를 기준으로 찾기
      */
-    Optional<Answer> findByAnswerId(Long id);
+    Optional<Answer> findById(Long id);
 
     /**
      * 답변 저장
      */
     Answer save(Answer answer, Question question);
+
+    /**
+     * 답변 수정
+     */
+    void edit(Answer answer);
+
+    /**
+     * 답변 삭제
+     */
+    void deleteById(Long id);
 }
