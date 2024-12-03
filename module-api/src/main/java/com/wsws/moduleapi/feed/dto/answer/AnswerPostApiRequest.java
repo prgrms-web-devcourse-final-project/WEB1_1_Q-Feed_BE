b4bd5b7b699dc90ffc18e026c0bcacd3edb486b7
jhk01007 <jhk001007@naver.com>
2024-12-03 10:48:38 +0900

    [QFEED-81] Refactor: 검증 어노테이션 추가

--- module-api/src/main/java/com/wsws/moduleapi/feed/dto/answer/AnswerPostApiRequest.java
+++ module-api/src/main/java/com/wsws/moduleapi/feed/dto/answer/AnswerPostApiRequest.java
@@ -1,12 +1,16 @@
 package com.wsws.moduleapi.feed.dto.answer;
 
 import com.wsws.moduleapplication.feed.dto.AnswerCreateServiceRequest;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 public record AnswerPostApiRequest(
+        @NotBlank(message = "question id는 필수 값입니다.")
         Long questionId,
         String content,
         MultipartFile image,
+        @NotNull(message = "visibility는 필수 값입니다.")
         Boolean visibility
 ) {
     public AnswerCreateServiceRequest toServiceDto(String userId) {
