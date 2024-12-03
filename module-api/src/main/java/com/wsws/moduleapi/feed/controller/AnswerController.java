245d8b55bad63f8aa8e51d8b3d3c26812ec9fc56
jhk01007 <jhk001007@naver.com>
2024-12-03 10:32:47 +0900

    [QFEED-81] Test: 테스트를 위한 임시 수정

--- module-api/src/main/java/com/wsws/moduleapi/feed/controller/AnswerController.java
+++ module-api/src/main/java/com/wsws/moduleapi/feed/controller/AnswerController.java
@@ -35,10 +35,11 @@ public class AnswerController {
      */
     @PostMapping
     public ResponseEntity<AnswerPostApiResponse> postAnswers(
-            @RequestBody AnswerPostApiRequest answerPostApiRequest,
-            @AuthenticationPrincipal UserPrincipal userPrincipal
+            @RequestBody AnswerPostApiRequest answerPostApiRequest
+            ,@AuthenticationPrincipal UserPrincipal userPrincipal
     ) {
         String userId = userPrincipal.getId(); // 사용자 아이디를 가져온다.
+//        String userId = "user_id1";
         AnswerCreateServiceResponse serviceResponse = answerService.createAnswer(answerPostApiRequest.toServiceDto(userId)); // 답변 생성
 
         return ResponseEntity.status(201).body(new AnswerPostApiResponse(serviceResponse));
