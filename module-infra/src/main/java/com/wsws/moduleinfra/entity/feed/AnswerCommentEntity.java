295c36df7e9777b904f8f847cc73e4fb83175965
jhk01007 <jhk001007@naver.com>
2024-12-04 22:15:15 +0900

    [QFEED-101] Feat: AnswerCommentEntity 추가

---
+++ module-infra/src/main/java/com/wsws/moduleinfra/entity/feed/AnswerCommentEntity.java
@@ -0,0 +1,40 @@
+package com.wsws.moduleinfra.entity.feed;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "answer_comment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AnswerCommentEntity {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String content;
+    private int depth;
+    private int reactionCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id")
+    private AnswerEntity answerEntity;
+
+    private String userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private AnswerCommentEntity parentCommentEntity;
+
+    public static AnswerCommentEntity create(String content, int depth, int reactionCount, AnswerEntity answerEntity, String userId, AnswerCommentEntity parentCommentEntity) {
+        AnswerCommentEntity answerCommentEntity = new AnswerCommentEntity();
+        answerCommentEntity.content = content;
+        answerCommentEntity.depth = depth;
+        answerCommentEntity.reactionCount = reactionCount;
+        answerCommentEntity.answerEntity = answerEntity;
+        answerCommentEntity.userId = userId;
+        answerCommentEntity.parentCommentEntity = parentCommentEntity;
+
+        return answerCommentEntity;
+    }
+
+}
