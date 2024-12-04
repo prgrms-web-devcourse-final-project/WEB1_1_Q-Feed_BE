9c3a9ee05b94019fd9b59392d083ef95e890aa10
김지은 <wldms5119@naver.com>
2024-12-04 21:22:51 +0900

    feature : fcm 알림 전송 구현

--- module-external-api/src/main/java/com/wsws/moduleexternalapi/fcm/service/FcmService.java
+++ module-external-api/src/main/java/com/wsws/moduleexternalapi/fcm/service/FcmService.java
@@ -8,7 +8,7 @@ import com.wsws.moduledomain.user.User;
 import com.wsws.moduledomain.user.repo.UserRepository;
 import com.wsws.moduledomain.user.vo.UserId;
 import com.wsws.moduleexternalapi.fcm.dto.FCMRequestDto;
-//import com.wsws.moduleinfra.FcmRedis;
+import com.wsws.moduleinfra.FcmRedis;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -20,29 +20,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FcmService {
 
-//      private final FcmRedis fcmRedis;
+      private final FcmRedis fcmRedis;
       private final UserRepository userRepository;
 
-//    @Async("taskExecutor")
-//    public void FcmSend(String recipient, FCMRequestDto fcmRequestDto) {
-//        User user = getUserById(recipient); // 사용자 조회
-//        UserId userId = user.getId(); // UserId 가져오기
-//
-//        String fcmRedisKey = getFcmRedisKey(recipient); // Redis 키 생성.
-//        String fcmToken = fcmRedis.getFcmToken(fcmRedisKey); // Redis에서 FCM 토큰 조회.
-//
-//        if (fcmToken != null && !fcmToken.isEmpty()) { // 토큰이 존재하면 메시지 생성 후 전송.
-//            Message message = makeMessage(fcmRequestDto, fcmToken);
-//            sendMessage(message);
-//        }
-//    }
+    @Async("taskExecutor")
+    public void FcmSend(String recipient, FCMRequestDto fcmRequestDto) {
 
-    private User getUserById(String recipient) {
-        return userRepository.findById(UserId.of(recipient))
-                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 사용자 ID: " + recipient));
+        String fcmRedisKey = getFcmRedisKey(recipient); // Redis 키 생성.
+        String fcmToken = fcmRedis.getFcmToken(fcmRedisKey); // Redis에서 FCM 토큰 조회.
+
+        if (fcmToken != null && !fcmToken.isEmpty()) { // 토큰이 존재하면 메시지 생성 후 전송.
+            Message message = makeMessage(fcmRequestDto, fcmToken);
+            sendMessage(message);
+        }
     }
-
-
+    
     // 메시지 생성
     public Message makeMessage(FCMRequestDto fcmRequestDto, String token){ // 나중에 토큰도 추가
         Notification.Builder notificationBuilder =
