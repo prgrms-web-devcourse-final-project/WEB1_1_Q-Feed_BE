82d76e1f739ca44df0cc4874a86a7d46cbd17e94
LeeHyeonseob <hyeonseob22@gmail.com>
2024-12-04 09:42:19 +0900

    [QFEED-100] Fix: api 경로 수정

--- module-api/src/main/java/com/wsws/moduleapi/recommendation/RecommendationController.java
+++ module-api/src/main/java/com/wsws/moduleapi/recommendation/RecommendationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/recommendations")
+@RequestMapping("/recommendations")
 @RequiredArgsConstructor
 public class RecommendationController {
 
