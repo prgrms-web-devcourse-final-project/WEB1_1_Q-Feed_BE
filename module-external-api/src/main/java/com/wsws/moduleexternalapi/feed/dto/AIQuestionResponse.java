e54f2876a9bb183a08d1c88271712b148dc6f86b
jhk01007 <jhk001007@naver.com>
2024-12-06 17:39:40 +0900

    [QFEED-101] Fix: 카테고리 변경으로 인한 오류 해결

--- module-external-api/src/main/java/com/wsws/moduleexternalapi/feed/dto/AIQuestionResponse.java
+++ module-external-api/src/main/java/com/wsws/moduleexternalapi/feed/dto/AIQuestionResponse.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record AIQuestionResponse(
         @JsonProperty(required = true, value = "TRAVEL") String travel,
         @JsonProperty(required = true, value = "DELICIOUS_RESTAURANT") String deliciousRestaurant,
-        @JsonProperty(required = true, value = "MOVIE") String movie,
-        @JsonProperty(required = true, value = "MUSIC") String music,
-        @JsonProperty(required = true, value = "READING") String reading,
-        @JsonProperty(required = true, value = "SPORTS") String sports) {
+        @JsonProperty(required = true, value = "FASHION") String fashion,
+        @JsonProperty(required = true, value = "CULTURE") String culture,
+        @JsonProperty(required = true, value = "SPORTS") String sports,
+        @JsonProperty(required = true, value = "ETC") String etc) {
 }
 
