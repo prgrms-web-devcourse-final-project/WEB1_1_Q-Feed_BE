a349214ace83a8502392670faa59702d54352ded
jhk01007 <jhk001007@naver.com>
2024-12-06 16:50:29 +0900

    [QFEED-101] Fix: 테스트용 코드 삭제

--- module-api/src/main/java/com/wsws/moduleapi/feed/controller/AnswerController.java
+++ module-api/src/main/java/com/wsws/moduleapi/feed/controller/AnswerController.java
@@ -41,13 +41,13 @@ public class AnswerController {
             @ApiResponse(responseCode = "404", description = "작성자 정보를 찾을 수 없을 때", content = @Content)
     })
     public ResponseEntity<AnswerListGetApiResponse> getAnswerList(
-//            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
             @Parameter(description = "커서로 사용할 마지막 글의 시간", example = "2024-01-01T00:00:00") @RequestParam(required = false) String answerCursor,
             @Parameter(description = "페이지 크기", example = "10") @RequestParam(defaultValue = "10") int size
     ) {
 
-//        String userId = userPrincipal.getId();
-        String userId = "user_id1";
+        String userId = userPrincipal.getId();
+//        String userId = "user_id1";
         LocalDateTime parsedCursor = answerCursor != null ? LocalDateTime.parse(answerCursor) : LocalDateTime.now();
         AnswerListFindServiceResponse answers = answerReadService.findAnswerListWithCursor(new AnswerFindServiceRequest(userId, null, parsedCursor, size));
 
@@ -67,13 +67,13 @@ public class AnswerController {
             @ApiResponse(responseCode = "404", description = "작성자 정보를 찾을 수 없을 때", content = @Content)
     })
     public ResponseEntity<AnswerGetApiResponse> getAnswers(
-//            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
             @Parameter(description = "답변 상세를 조회할 답변 ID") @PathVariable("answer-id") Long answerId,
             @Parameter(description = "커서로 사용할 마지막 댓글의 시간", example = "2024-01-01T00:00:00") @RequestParam(required = false) String commentCursor,
             @Parameter(description = "페이지 크기", example = "10") @RequestParam(defaultValue = "2") int size
     ) {
-//        String userId = userPrincipal.getId();
-        String userId = "user_id1";
+        String userId = userPrincipal.getId();
+//        String userId = "user_id1";
 
         LocalDateTime parsedCursor = commentCursor != null ? LocalDateTime.parse(commentCursor) : LocalDateTime.now();
 
