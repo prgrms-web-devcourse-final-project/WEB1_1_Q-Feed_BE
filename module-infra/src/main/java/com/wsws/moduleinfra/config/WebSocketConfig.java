6d2fde95b607f8d75f7ddd62aa45c6697336a43d
wjdwltn <jlsowltn2000@naver.com>
2024-12-08 02:20:14 +0900

    [QFEED-45] Chore: 웹소켓 test.html 추가

--- module-infra/src/main/java/com/wsws/moduleinfra/config/WebSocketConfig.java
+++ module-infra/src/main/java/com/wsws/moduleinfra/config/WebSocketConfig.java
@@ -11,9 +11,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry endpointRegistry) {
-        endpointRegistry.addEndpoint("/ws")
-                .setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
