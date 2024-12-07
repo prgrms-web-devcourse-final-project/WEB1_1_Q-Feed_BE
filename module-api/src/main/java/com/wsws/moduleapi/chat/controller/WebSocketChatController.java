8ecd9caad9437be086d207f4e9705885c673e1c2
wjdwltn <jlsowltn2000@naver.com>
2024-12-07 22:58:35 +0900

    [QFEED-45] Hotfix: 웹소켓 테스트 변경..

--- module-api/src/main/java/com/wsws/moduleapi/chat/controller/WebSocketChatController.java
+++ module-api/src/main/java/com/wsws/moduleapi/chat/controller/WebSocketChatController.java
@@ -16,8 +16,8 @@ public class WebSocketChatController {
     {
         System.out.println("서버에서 받은 메시지: " + webSocketChatRequest);
         chatMessageService.sendMessage(
-                webSocketChatRequest.roomId(),
-                webSocketChatRequest.senderId(),
+                1L,
+                "18312fd3-a56f-4b91-80d2-dab72c584857",
                 webSocketChatRequest.toChatMessageRequest()
         );
     }
