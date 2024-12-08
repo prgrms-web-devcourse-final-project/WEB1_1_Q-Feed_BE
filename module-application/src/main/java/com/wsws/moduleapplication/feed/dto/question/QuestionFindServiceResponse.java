0de2e2b4b9e6b303ee6bc18182b7b1a9207b2c20
jhk01007 <jhk001007@naver.com>
2024-12-08 16:24:49 +0900

    [QFEED-129] Feat: 답변 조회 API 응답에 createdAt 추가

--- module-application/src/main/java/com/wsws/moduleapplication/feed/dto/question/QuestionFindServiceResponse.java
+++ module-application/src/main/java/com/wsws/moduleapplication/feed/dto/question/QuestionFindServiceResponse.java
@@ -2,15 +2,19 @@ package com.wsws.moduleapplication.feed.dto.question;
 
 import com.wsws.moduledomain.feed.question.Question;
 
+import java.time.LocalDateTime;
+
 public record QuestionFindServiceResponse(
         Long questionId,
-        String content
+        String content,
+        LocalDateTime createdAt
 ) {
 
     public QuestionFindServiceResponse(Question question) {
         this(
                 question.getQuestionId().getValue(),
-                question.getContent()
+                question.getContent(),
+                question.getCreatedAt()
         );
     }
 }
