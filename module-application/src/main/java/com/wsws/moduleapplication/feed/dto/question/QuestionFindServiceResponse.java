a00cc77c96597db9c70803ef04ce9625b943d5cd
jhk01007 <jhk001007@naver.com>
2024-12-16 13:09:35 +0900

    [QFEED-143] Fix: Jackson이 LocalDate를 직렬화하지 못하는 현상 해결

--- module-application/src/main/java/com/wsws/moduleapplication/feed/dto/question/QuestionFindServiceResponse.java
+++ module-application/src/main/java/com/wsws/moduleapplication/feed/dto/question/QuestionFindServiceResponse.java
@@ -1,11 +1,17 @@
 package com.wsws.moduleapplication.feed.dto.question;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.wsws.moduledomain.feed.question.Question;
 import java.time.LocalDate;
 
 public record QuestionFindServiceResponse(
         Long questionId,
         String content,
+        @JsonSerialize(using = LocalDateSerializer.class)
+        @JsonDeserialize(using = LocalDateDeserializer.class)
         LocalDate questionDate
 ) {
 
