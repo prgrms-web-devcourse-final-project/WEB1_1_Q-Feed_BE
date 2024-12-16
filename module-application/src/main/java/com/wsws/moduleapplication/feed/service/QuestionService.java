7579796e1c41d0a3f63a3f1485aee5e36c8b2cee
jhk01007 <jhk001007@naver.com>
2024-12-16 13:34:41 +0900

    [QFEED-143] Refactor: 캐시에 도메인을 저장하던 부분을 DTO 저장으로 수정

--- module-application/src/main/java/com/wsws/moduleapplication/feed/service/QuestionService.java
+++ module-application/src/main/java/com/wsws/moduleapplication/feed/service/QuestionService.java
@@ -6,12 +6,14 @@ import com.wsws.moduledomain.cache.CacheManager;
 import com.wsws.moduledomain.feed.question.Question;
 import com.wsws.moduledomain.feed.question.repo.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
@@ -25,20 +27,24 @@ public class QuestionService {
         // 캐시에서 데이터 조회
         String questionCacheKey = "QUESTION:" + LocalDate.now() + ":CATEGORY:" + categoryId; // 예: QUESTION:2024-12-16:CATEGORY:1
 
-        Question cachedQuestion = cacheManager.get(questionCacheKey, Question.class);
+        QuestionFindServiceResponse cachedQuestion = cacheManager.get(questionCacheKey, QuestionFindServiceResponse.class);
 
         // 캐시에 데이터가 있다면 해당 데이터 반환
-        if (cachedQuestion != null)
-            return new QuestionFindServiceResponse(cachedQuestion);
+        if (cachedQuestion != null) {
+            log.info("cache hit: {}", cachedQuestion);
+            return cachedQuestion;
+        }
 
         Question question = questionRepository.findDailyQuestionByCategoryId(categoryId)
                 .orElseThrow(() -> QuestionNotFoundException.EXCEPTION);
 
+        QuestionFindServiceResponse serviceResponse = new QuestionFindServiceResponse(question);
+
         // 캐시에 데이터가 없다면 캐시에 해당 Question 저장
         if(question.getQuestionDate().equals(LocalDate.now())) // 만약 질문 생성이 실패된 경우, 어제 질문이 오늘의 질문으로 캐시에 저장되는 현상을 방지
-            cacheManager.set(questionCacheKey, question, 24 * 60);
+            cacheManager.set(questionCacheKey, serviceResponse, 24 * 60);
 
-        return new QuestionFindServiceResponse(question);
+        return serviceResponse;
     }
 
 }
