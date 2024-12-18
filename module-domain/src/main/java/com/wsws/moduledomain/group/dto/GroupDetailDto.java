8ed058995b1c2668c82366b3d389fa19650abc17
wjdwltn <jlsowltn2000@naver.com>
2024-12-18 16:23:20 +0900

    [QFEED-59] Chore : 그룹 상세 페이지 categoryId 추가

--- module-domain/src/main/java/com/wsws/moduledomain/group/dto/GroupDetailDto.java
+++ module-domain/src/main/java/com/wsws/moduledomain/group/dto/GroupDetailDto.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 public record GroupDetailDto(
         Long groupId,
+        Long categoryId,
         CategoryName categoryName,
         String url,
         String groupName,
