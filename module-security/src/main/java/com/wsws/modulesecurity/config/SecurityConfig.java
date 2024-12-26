9b0149e4893aef59d485f8c01e6666eaba78ec08
wjdwltn <jlsowltn2000@naver.com>
2024-12-27 00:07:03 +0900

    hotfix : test2 권한 허용

--- module-security/src/main/java/com/wsws/modulesecurity/config/SecurityConfig.java
+++ module-security/src/main/java/com/wsws/modulesecurity/config/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
 //                        .requestMatchers("/feed/**").permitAll() // 테스트용 임시 추가
                         .requestMatchers("/chats/**").permitAll()
                         .requestMatchers("/test.html").permitAll()
+                        .requestMatchers("/test2.html").permitAll()
                         .requestMatchers("/groups/**").permitAll()
                         .requestMatchers("/ws/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN") // 관리자 전용 엔드포인트(나중에 구현)
