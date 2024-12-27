9298f4bf8b82d973b5bc82ff0e1590ee72e13971
LeeHyeonseob <hyeonseob22@gmail.com>
2024-12-27 12:29:50 +0900

    [QFEED-150] Feature: swagger 설명 추가

--- module-api/src/main/java/com/wsws/moduleapi/admin/controller/AdminController.java
+++ module-api/src/main/java/com/wsws/moduleapi/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.wsws.moduleapi.admin.controller;
 
 import com.wsws.moduleapplication.admin.service.AdminService;
 import com.wsws.moduleapplication.report.dto.ReportDetails;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ public class AdminController {
     private final AdminService adminService;
 
     //신고 목록 조회
+    @Operation(summary = "신고 목록 조회", description = "모든 신고 목록을 조회합니다.")
     @GetMapping("/report")
     public ResponseEntity<?> getAllReports(){
         List<ReportDetails> reportList = adminService.getReportList();
@@ -23,6 +25,7 @@ public class AdminController {
     }
 
     // 사용자 비활성화
+    @Operation(summary = "사용자 비활성화", description = "해당 사용자의 계정을 비활성화합니다.")
     @PatchMapping("/users/{userId}/deactive")
     public ResponseEntity<?> deactiveUser(@PathVariable String userId){
         adminService.deactivateUser(userId);
@@ -30,6 +33,7 @@ public class AdminController {
     }
 
     //사용자 활성화
+    @Operation(summary = "사용자 활성화", description = "해당 사용자의 계정을 활성화합니다.")
     @PatchMapping("/users/{userId}/active")
     public ResponseEntity<?> activeUser(@PathVariable String userId){
         adminService.activateUser(userId);
@@ -37,6 +41,7 @@ public class AdminController {
     }
 
     // 사용자 누적 신고 횟수 조회
+    @Operation(summary = "사용자 신고 누적 횟수 조회", description = "해당 사용자의 신고 누적 횟수를 조회합니다.")
     @GetMapping("/users/{userId}/count")
     public ResponseEntity<Long> getUserCount(@PathVariable String userId){
         Long reportCount = adminService.getReportCount(userId);
--- module-api/src/main/java/com/wsws/moduleapi/report/controller/ReportController.java
+++ module-api/src/main/java/com/wsws/moduleapi/report/controller/ReportController.java
@@ -6,6 +6,7 @@ import com.wsws.moduleapplication.report.dto.CreateReportRequest;
 import com.wsws.moduleapplication.report.dto.ReportDetails;
 import com.wsws.moduleapplication.report.service.ReportService;
 import com.wsws.modulesecurity.security.UserPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,7 @@ public class ReportController {
 
     //신고 생성
     @PostMapping
+    @Operation(summary = "신고 생성", description = "특정 사용자를 신고합니다")
     public ResponseEntity<Void> report(@RequestBody ReportRequest request, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         String userId = userPrincipal.getId();
         CreateReportRequest serviceRequest = reportMapper.toCreateReportRequest(request, userId);
@@ -32,6 +34,7 @@ public class ReportController {
     }
 
     //사용자 신고 조회
+    @Operation(summary = "사용자 신고 내역 조회", description = "해당 사용자의 신고 내열을 조회합니다.")
     @GetMapping("/{userId}")
     public ResponseEntity<List<ReportDetails>> getUserReports(@PathVariable String userId){
         List<ReportDetails> reports = reportService.getReportsByReportedUserId(userId);
