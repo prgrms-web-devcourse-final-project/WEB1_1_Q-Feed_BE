d2962ea65a570f1327f4cec3abc617b2adb6c705
LeeHyeonseob <hyeonseob22@gmail.com>
2024-12-28 22:28:39 +0900

    [QFEED-157] Refactor: 이메일 비동기 처리

--- module-infra/src/main/java/com/wsws/moduleinfra/authcontext/auth/service/EmailServiceImpl.java
+++ module-infra/src/main/java/com/wsws/moduleinfra/authcontext/auth/service/EmailServiceImpl.java
@@ -5,6 +5,7 @@ import com.wsws.moduledomain.authcontext.auth.repo.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +15,7 @@ public class EmailServiceImpl implements EmailService {
     private final JavaMailSender mailSender;
 
     @Override
+    @Async
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
