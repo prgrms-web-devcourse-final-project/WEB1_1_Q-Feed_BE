af49bf5b5b8fe1150385a56a9f18b751f2e9e968
김지은 <wldms5119@naver.com>
2024-12-31 00:34:13 +0900

    chore:  Base64 인코딩된 Firebase 서비스 키를 사용하도록 FcmInitializer 및 CI/CD 파이프라인 수정

--- module-external-api/src/main/java/com/wsws/moduleexternalapi/fcm/config/FcmInitializer.java
+++ module-external-api/src/main/java/com/wsws/moduleexternalapi/fcm/config/FcmInitializer.java
@@ -5,7 +5,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -14,13 +14,14 @@ import java.io.IOException;
 @Slf4j
 public class FcmInitializer {
 
-    private static final String FIREBASE_CONFIG_PATH = "firebase/fcmSecret.json";
+    private static final String FIREBASE_CONFIG_PATH = "module-external-api/src/main/resources/firebase/firebase-service-key.json";
 
     @PostConstruct
     public void initialize() {
         try {
+            FileSystemResource resource = new FileSystemResource(FIREBASE_CONFIG_PATH);
             GoogleCredentials googleCredentials = GoogleCredentials
-                    .fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream());
+                    .fromStream(resource.getInputStream());
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(googleCredentials)
                     .build();
