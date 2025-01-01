6b33c1cdfcb642b098377e182bd7ea160d61fa3a
김지은 <wldms5119@naver.com>
2025-01-02 02:05:37 +0900

    fix: Firebase 경로 클래스패스 사용하도록 수정

--- module-external-api/src/main/java/com/wsws/moduleexternalapi/fcm/config/FcmInitializer.java
+++ module-external-api/src/main/java/com/wsws/moduleexternalapi/fcm/config/FcmInitializer.java
@@ -6,42 +6,34 @@ import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @Component
 @Slf4j
 public class FcmInitializer {
 
-    private static final String FIREBASE_CONFIG_PATH = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
-
     @PostConstruct
     public void initialize() {
         try {
-            Resource resource;
+            log.info("Firebase 서비스 계정 키 파일을 로드합니다.");
+            ClassPathResource resource = new ClassPathResource("firebase/firebase-service-key.json");
 
-            if (FIREBASE_CONFIG_PATH != null && !FIREBASE_CONFIG_PATH.isEmpty()) {
-                log.info("환경 변수에서 Firebase 설정 파일 경로를 읽어옵니다: {}", FIREBASE_CONFIG_PATH);
-                resource = new FileSystemResource(FIREBASE_CONFIG_PATH);
-            } else {
-                log.info("JAR 내부의 Firebase 설정 파일을 읽어옵니다.");
-                resource = new ClassPathResource("firebase/firebase-service-key.json");
-            }
+            try (InputStream serviceAccountStream = resource.getInputStream()) {
+                GoogleCredentials googleCredentials = GoogleCredentials.fromStream(serviceAccountStream);
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(googleCredentials)
+                        .build();
 
-            GoogleCredentials googleCredentials = GoogleCredentials.fromStream(resource.getInputStream());
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(googleCredentials)
-                    .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-                log.info("Firebase 초기화 완료");
+                if (FirebaseApp.getApps().isEmpty()) {
+                    FirebaseApp.initializeApp(options);
+                    log.info("Firebase 초기화가 완료되었습니다.");
+                }
             }
         } catch (IOException e) {
-            log.error("Firebase 초기화 실패: {}", e.getMessage());
+            log.error("Firebase 초기화 중 오류 발생: {}", e.getMessage());
             throw new IllegalStateException("Firebase 초기화 실패", e);
         }
     }
