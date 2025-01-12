f346baa5d43df335dfc50bd987cc1b9cf382aa40
김지은 <wldms5119@naver.com>
2025-01-13 04:01:10 +0900

    [QFEED-147] fix : fix: Redis 직렬화로 잘못된 값 저장되는 문제 StringRedisTemplate 적용하여 해결

--- module-infra/src/main/java/com/wsws/moduleinfra/FcmRedis.java
+++ module-infra/src/main/java/com/wsws/moduleinfra/FcmRedis.java
@@ -1,8 +1,7 @@
 package com.wsws.moduleinfra;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import java.time.Duration;
 
@@ -10,22 +9,21 @@ import java.time.Duration;
 @RequiredArgsConstructor
 public class FcmRedis {
 
-    @Qualifier("customRedisTemplateObject")
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final StringRedisTemplate stringRedisTemplate;
 
     private static final String PREFIX = "FCM_TOKEN_";
 
     public void saveFcmToken(String userId, String fcmToken, Duration time) {
         String key = PREFIX + userId;
-        redisTemplate.opsForValue().set(key, fcmToken, time);
+        stringRedisTemplate.opsForValue().set(key, fcmToken, time);
     }
 
     public String getFcmToken(String redisKey) {
-        return (String) redisTemplate.opsForValue().get(redisKey);
+        return stringRedisTemplate.opsForValue().get(redisKey);
     }
 
     public void deleteFcmToken(String userId) {
         String key = PREFIX + userId;
-        redisTemplate.delete(key);
+        stringRedisTemplate.delete(key);
     }
 }
