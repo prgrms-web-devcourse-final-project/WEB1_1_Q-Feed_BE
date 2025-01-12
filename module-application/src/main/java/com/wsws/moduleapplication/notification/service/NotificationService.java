ec1662bdd50d83e344caf108da4c780a98ea46d3
김지은 <wldms5119@naver.com>
2025-01-13 04:08:25 +0900

    [QFEED-147] fix: FCM 메시지 전송에서 SDK 작동 문제 해결 (OAuth 2.0 AccessToken 방식으로 전환) - 기존 Firebase SDK 방식에서 발생한 작동 문제 해결 - OAuth 2.0 AccessToken 방식으로 전송 로직 변경 - AccessTokenUtil 추가 - RestTemplate를 사용하여 FCM API 호출 - FcmType 및 메시지 생성 로직 수정

--- module-application/src/main/java/com/wsws/moduleapplication/notification/service/NotificationService.java
+++ module-application/src/main/java/com/wsws/moduleapplication/notification/service/NotificationService.java
@@ -10,10 +10,9 @@ import com.wsws.moduledomain.notification.repo.NotificationRepository;
 import com.wsws.moduledomain.usercontext.user.aggregate.User;
 import com.wsws.moduledomain.usercontext.user.repo.UserRepository;
 import com.wsws.moduledomain.usercontext.user.vo.UserId;
-import com.wsws.moduleexternalapi.fcm.dto.fcmRequestDto;
 import com.wsws.moduleexternalapi.fcm.service.FcmService;
-import com.wsws.moduleexternalapi.fcm.util.FcmType;
 import com.wsws.moduleinfra.FcmRedis;
+import com.wsws.moduleexternalapi.fcm.util.FcmType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -34,10 +33,8 @@ public class NotificationService {
     private final UserRepository userRepository;
     private final FcmRedis fcmRedis;
 
-    // ID 생성
     private final AtomicLong notificationIdGenerator = new AtomicLong(1);
 
-    // 모든 알림 목록 출력 (읽음/안읽음 포함)
     @Transactional
     public List<NotificationServiceResponse> getNotifications(String recipientId) {
         return notificationRepository.findByRecipientId(recipientId).stream()
@@ -45,7 +42,6 @@ public class NotificationService {
                 .toList();
     }
 
-    // 개별 읽음 처리
     @Transactional
     public void markAsRead(Long notificationId) {
         Notification notification = notificationRepository.findById(notificationId)
@@ -56,11 +52,9 @@ public class NotificationService {
         }
 
         notification.markAsRead();
-        // 읽음 상태 반영
         notificationRepository.edit(notification);
     }
 
-    // 전체 읽음 처리
     @Transactional
     public void markAllAsRead(String recipientId) {
         List<NotificationDto> unreadNotifications = notificationRepository.findByRecipientIdAndIsReadFalse(recipientId);
@@ -72,32 +66,28 @@ public class NotificationService {
         notificationRepository.markAllAsReadByRecipientId(recipientId);
     }
 
-    // 알림 생성 및 저장
     public void sendNotification(String senderId, String recipientId, Long targetId, Long commentId, Long groupId, String url, FcmType fcmType) {
         User sender = userRepository.findById(UserId.of(senderId))
                 .orElseThrow(() -> SenderNotFoundException.EXCEPTION);
         User recipient = userRepository.findById(UserId.of(recipientId))
                 .orElseThrow(() -> RecipientNotFoundException.EXCEPTION);
 
-
         Long notificationId = notificationIdGenerator.getAndIncrement();
 
-
-        // 알림 내용 생성
-        String title = fcmService.makeFcmTitle(fcmType.getType());
-        String body = createNotificationBody(fcmType, sender.getNickname().getValue());
-
-        fcmRequestDto fcmDTO = fcmService.makeFcmDTO(title, body);
-
         // FCM 전송
-        fcmService.fcmSend(sender.getNickname().getValue(), fcmDTO);
+        fcmService.fcmSend(
+                recipient.getId().getValue(),
+                fcmType,
+                sender.getNickname().getValue()
+        );
+
         // 알림 저장
         Notification notification = Notification.create(
                 notificationId,
-                fcmType.getType(),
+                fcmType.name(),
                 sender.getId().getValue(),
                 recipient.getId().getValue(),
-                fcmDTO.body(), // FCM 전송 body
+                fcmType.name(),
                 targetId,
                 commentId,
                 groupId,
@@ -106,52 +96,26 @@ public class NotificationService {
         notificationRepository.save(notification);
     }
 
-    // FcmType 에 따른 알림 본문 생성
-    private String createNotificationBody(FcmType fcmType, String sender) {
-        return switch (fcmType) {
-            case FOLLOW -> fcmService.makeFollowBody(sender, fcmType.getType());
-            case CHAT -> fcmService.makeChatBody(sender, fcmType.getType());
-            case ANSWER_COMMENT -> fcmService.makeCommentBody(sender, fcmType.getType());
-            case ANSWER_LIKE -> fcmService.makeAnswerLikeBody(sender, fcmType.getType());
-            case COMMENT_LIKE -> fcmService.makeCommentLikeBody(sender, fcmType.getType());
-            case Q_SPACE_POST_COMMENT -> fcmService.makeQCommentBody(sender, fcmType.getType());
-            case Q_SPACE_POST_LIKE -> fcmService.makeQPostLikeBody(sender, fcmType.getType());
-            case Q_SPACE_COMMENT_LIKE -> fcmService.makeQCommentLikeBody(sender, fcmType.getType());
-        };
-    }
-
     public void saveFcmToken(SaveFcmTokenRequest request, String userId) {
         User user = userRepository.findById(UserId.of(userId))
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
         String value = request.fcmToken();
-        Duration twoMonths = Duration.ofDays(60); // 2달
+        Duration twoMonths = Duration.ofDays(60);
 
-        // Redis 저장 시도
-        log.info("Redis에 FCM 토큰 저장 시도!! userId={}, fcmToken={}", userId, value);
-
-        fcmRedis.saveFcmToken(String.valueOf(user.getId()), value, twoMonths);
-
-        // Redis 저장 완료
-        log.info("Redis에 FCM 토큰 저장 완료!! userId={}, fcmToken={}", userId, value);
+        log.info("Redis에 FCM 토큰 저장 시도: userId={}, fcmToken={}", userId, value);
+        fcmRedis.saveFcmToken(user.getId().getValue(), value, twoMonths);
+        log.info("Redis에 FCM 토큰 저장 완료: userId={}, fcmToken={}", userId, value);
     }
 
-    // FCM 토큰 삭제 로직
     public void deleteFcmToken(String userId) {
-
-        // Redis에서 토큰 삭제
         fcmRedis.deleteFcmToken(String.valueOf(userId));
-        log.info("FCM 토큰 삭제 완료 > userId={}", userId);
+        log.info("FCM 토큰 삭제 완료: userId={}", userId);
 
-        // Redis에서 해당 토큰이 존재하는지 확인 (삭제확인 테스트용)`
         String token = fcmRedis.getFcmToken(String.valueOf(userId));
         if (token == null) {
-            log.info("FCM 토큰 삭제 확인 완료 > userId={}", userId);
+            log.info("FCM 토큰 삭제 확인 완료: userId={}", userId);
         } else {
-            log.warn("FCM 토큰 삭제 실패 > userId={}, token={}", userId, token);
+            log.warn("FCM 토큰 삭제 실패: userId={}, token={}", userId, token);
         }
     }
-
-
-
-
 }
--- module-external-api/src/main/java/com/wsws/moduleexternalapi/fcm/util/FcmType.java
+++ module-external-api/src/main/java/com/wsws/moduleexternalapi/fcm/util/FcmType.java
@@ -6,13 +6,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum FcmType {
-    FOLLOW("팔로우"),
-    CHAT("채팅"),
-    ANSWER_COMMENT("[Answer]댓글"),
-    ANSWER_LIKE("[Answer]좋아요"),
-    COMMENT_LIKE("[Comment]좋아요"),
-    Q_SPACE_POST_COMMENT("[Q_post]댓글"),
-    Q_SPACE_POST_LIKE("[Q_post]좋아요"),
-    Q_SPACE_COMMENT_LIKE("[Q_comment]좋아요");
-    private final String type;
+    FOLLOW,
+    ANSWER_COMMENT,
+    ANSWER_LIKE,
+    COMMENT_LIKE,
+    Q_SPACE_POST_COMMENT,
+    Q_SPACE_POST_LIKE,
+    Q_SPACE_COMMENT_LIKE;
+
 }
