6db87b21979535428f83748dbdb83668030da059
김지은 <wldms5119@naver.com>
2025-01-16 01:49:52 +0900

    [QFEED-147] feature: 알림 조회 시 알림 발신자 프로필 이미지 응답에 추가

--- module-domain/src/main/java/com/wsws/moduledomain/notification/dto/NotificationDto.java
+++ module-domain/src/main/java/com/wsws/moduledomain/notification/dto/NotificationDto.java
@@ -13,6 +13,7 @@ public record NotificationDto(
         String url,
         Long targetId, // 게시글 ID 또는 댓글이 달린 게시글 I
         Long commentId,// 댓글 ID
-        Long groupId  // 그룹 ID
-) {
+        Long groupId,  // 그룹 ID
+        String profileImage
+        ) {
 }
