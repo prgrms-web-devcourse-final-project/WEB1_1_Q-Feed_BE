98ebbf9a3df63b29c671f2cf2429a6cde3eb4fa9
wjdwltn <jlsowltn2000@naver.com>
2025-01-16 21:50:15 +0900

    [QFEED-152] fix: import 삭제

--- module-api/src/main/java/com/wsws/moduleapi/chat/controller/ChatMessageController.java
+++ module-api/src/main/java/com/wsws/moduleapi/chat/controller/ChatMessageController.java
@@ -1,19 +1,15 @@
 package com.wsws.moduleapi.chat.controller;
 
-import com.wsws.moduleapi.auth.dto.AuthResponse;
 import com.wsws.moduleapi.chat.dto.ChatMessageApiResponse;
 import com.wsws.moduleapi.chat.dto.ChatResponse;
 import com.wsws.moduleapplication.chat.dto.ChatMessageRequest;
 import com.wsws.moduleapplication.chat.dto.ChatMessageServiceResponse;
 import com.wsws.moduleapplication.chat.service.ChatMessageService;
-import com.wsws.moduleinfra.repo.chat.JpaChatMessageRepository;
 import com.wsws.modulesecurity.security.UserPrincipal;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,7 +17,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,19 +24,19 @@ import java.util.stream.Collectors;
 @SecurityRequirement(name = "bearerAuth") // Security 적용
 public class ChatMessageController {
     private final ChatMessageService chatMessageService;
-    private final JpaChatMessageRepository jpaChatMessageRepository;
 
-//    @PostMapping("/{chatRoomId}/send")
-//    @Operation(summary = "메세지 전송", description = "특정 채팅방에 메세지를 전송합니다.")
-//    public ResponseEntity<ChatResponse> sendMessage(
-//            @Parameter(description = "메세지를 전송할 채팅방 ID") @PathVariable Long chatRoomId,
-//            @RequestBody ChatMessageRequest request,
-//            @AuthenticationPrincipal UserPrincipal userPrincipal) {
-//
-//        String userId = userPrincipal.getId();
-//        chatMessageService.sendMessage(chatRoomId,userId,request);
-//        return ResponseEntity.status(201).body(new ChatResponse("메세지가 전송되었습니다."));
-//    }
+    @PostMapping("/{chatRoomId}/send")
+    @Operation(summary = "메세지 전송", description = "특정 채팅방에 메세지를 전송합니다.")
+    public ResponseEntity<ChatResponse> sendMessage(
+            @Parameter(description = "메세지를 전송할 채팅방 ID") @PathVariable Long chatRoomId,
+            @RequestBody ChatMessageRequest request,
+            @AuthenticationPrincipal UserPrincipal userPrincipal) {
+
+        String userId = userPrincipal.getId();
+        String receiverId = userPrincipal.getId();
+        chatMessageService.sendMessage(chatRoomId,userId,receiverId,request);
+        return ResponseEntity.status(201).body(new ChatResponse("메세지가 전송되었습니다."));
+    }
 
     // 채팅방의 메세지 조회 API
     @GetMapping("/{chatRoomId}/messages")
