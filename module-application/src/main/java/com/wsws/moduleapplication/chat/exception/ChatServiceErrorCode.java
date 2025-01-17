bc098b05a748cf25525a64e12109cb7fd6260196
wjdwltn <jlsowltn2000@naver.com>
2025-01-17 16:05:54 +0900

    [QFEED-152] Chore: ChatMessageService errorCode 수정

--- module-application/src/main/java/com/wsws/moduleapplication/chat/exception/ChatServiceErrorCode.java
+++ module-application/src/main/java/com/wsws/moduleapplication/chat/exception/ChatServiceErrorCode.java
@@ -10,12 +10,12 @@ import static com.wsws.modulecommon.constants.ErrorCodeConstants.*;
 public enum ChatServiceErrorCode implements BaseErrorCode {
     ALREADY_CHATROOM(BAD_REQUEST, "CHATROOM_400_1", "이미 존재하는 채팅방이 있습니다."),
     CHATROOM_NOT_FOUND(NOT_FOUND, "CHATROOM_400_2", "채팅방을 찾을 수 없습니다."),
-    CHAT_RECEIVER_NOT_FOUND(NOT_FOUND, "CHATROOM_400_2", "채팅중인 상대방을 찾을 수 없습니다."),
-    UNAUTHORIZED_ACCESS_CHAT(NOT_FOUND, "CHATROOM_400_2", "수정할 수 있는 권한이 없습니다."),
-    FILE_UPLOAD_REQUIRED(BAD_REQUEST, "CHATROOM_400_3","파일을 업로드해야 합니다."),
-    FILE_SIZE_EXCEEDED(BAD_REQUEST, "CHATROOM_400_4","파일 크기가 초과되었습니다."),
-    UNSUPPORTED_FILE_FORMAT(BAD_REQUEST, "CHATROOM_400_5","지원하지 않는 파일 형식입니다."),
-    FILE_PROCESSING_ERROR(INTERNAL_SERVER, "CHATROOM_400_6", "파일 처리 중 오류가 발생했습니다.");
+    CHAT_RECEIVER_NOT_FOUND(NOT_FOUND, "CHATROOM_400_3", "채팅중인 상대방을 찾을 수 없습니다."),
+    UNAUTHORIZED_ACCESS_CHAT(NOT_FOUND, "CHATROOM_400_4", "수정할 수 있는 권한이 없습니다."),
+    FILE_UPLOAD_REQUIRED(BAD_REQUEST, "CHATROOM_400_5","파일을 업로드해야 합니다."),
+    FILE_SIZE_EXCEEDED(BAD_REQUEST, "CHATROOM_400_6","파일 크기가 초과되었습니다."),
+    UNSUPPORTED_FILE_FORMAT(BAD_REQUEST, "CHATROOM_400_7","지원하지 않는 파일 형식입니다."),
+    FILE_PROCESSING_ERROR(INTERNAL_SERVER, "CHATROOM_400_8", "파일 처리 중 오류가 발생했습니다.");
 
     private final Integer status;
     private final String errorCode;
