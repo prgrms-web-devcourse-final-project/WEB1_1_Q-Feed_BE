cbb4d006cf0a65438e27bce45a11be939d9e8b98
jhk01007 <jhk001007@naver.com>
2025-01-18 14:46:53 +0900

    Refactor: 인기 답변 조회, 동적쿼리 Querydsl로 변경

--- module-infra/src/main/java/com/wsws/moduleinfra/repo/feed/AnswerRepositoryImpl.java
+++ module-infra/src/main/java/com/wsws/moduleinfra/repo/feed/AnswerRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.wsws.moduledomain.feed.answer.Answer;
 import com.wsws.moduledomain.feed.answer.repo.AnswerRepository;
 import com.wsws.moduledomain.feed.dto.AnswerQuestionDTO;
+import com.wsws.moduledomain.feed.question.vo.QuestionStatus;
 import com.wsws.moduleinfra.entity.feed.AnswerEntity;
 import com.wsws.moduleinfra.entity.feed.QuestionEntity;
 import com.wsws.moduleinfra.entity.feed.mapper.AnswerEntityMapper;
@@ -87,24 +88,6 @@ public class AnswerRepositoryImpl implements AnswerRepository {
                 .toList();
     }
 
-    private BooleanBuilder categoryIdEq(Long categoryId) {
-        BooleanBuilder booleanBuilder = new BooleanBuilder();
-        if (categoryId != null) {
-            booleanBuilder.and(questionEntity.categoryId.eq(categoryId));
-        }
-        return booleanBuilder;
-    }
-
-    private BooleanBuilder visibilityEqTrue(boolean isMine) {
-        BooleanBuilder builder = new BooleanBuilder();
-        if (!isMine) {
-            builder.and(answerEntity.visibility.eq(true));
-        }
-        return builder;
-    }
-
-
-    // TODO: 동적 쿼리 Querydsl로 수정
     @Override
     public Long countByUserId(String userId, boolean isMine) {
         return queryFactory
@@ -125,16 +108,22 @@ public class AnswerRepositoryImpl implements AnswerRepository {
     // TODO: 동적 쿼리 Querydsl로 수정
     @Override
     public List<Answer> findAnswersByLikeCountAndCategoryIdWithCursor(Long categoryId, int limit) {
-        Pageable pageable = PageRequest.of(0, limit); // 가져올 데이터 갯수 설정
-        return categoryId == null
-                ? jpaAnswerRepository.findAllOrderByLikeCountDescWithCursor(pageable).stream() // categoryId가 없다면 전체 조회
-                .map(AnswerEntityMapper::toDomain)
-                .toList()
-                : jpaAnswerRepository.findAllByCategoryIdOrderByLikeCountDescWithCursor(categoryId, pageable).stream() // categoryId가 있다면 해당 categoryId로 조회
+
+        List<AnswerEntity> answerEntities = queryFactory
+                .selectFrom(answerEntity)
+                .join(answerEntity.questionEntity, questionEntity)
+                .where(
+                        categoryIdEq(categoryId),
+                        questionEntity.questionStatus.eq(ACTIVATED)
+                ).orderBy(answerEntity.likeCount.desc())
+                .offset(0)
+                .limit(limit)
+                .fetch();
+
+        return answerEntities.stream()
                 .map(AnswerEntityMapper::toDomain)
                 .toList();
     }
-
     /**
      * 답변 저장
      */
@@ -173,5 +162,21 @@ public class AnswerRepositoryImpl implements AnswerRepository {
     @Override
     public void deleteById(Long id) {
         jpaAnswerRepository.deleteById(id);
+    }
+
+    private BooleanBuilder categoryIdEq(Long categoryId) {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        if (categoryId != null) {
+            booleanBuilder.and(questionEntity.categoryId.eq(categoryId));
+        }
+        return booleanBuilder;
+    }
+
+    private BooleanBuilder visibilityEqTrue(boolean isMine) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (!isMine) {
+            builder.and(answerEntity.visibility.eq(true));
+        }
+        return builder;
     }
 }
--- module-infra/src/main/java/com/wsws/moduleinfra/repo/feed/JpaAnswerRepository.java
+++ module-infra/src/main/java/com/wsws/moduleinfra/repo/feed/JpaAnswerRepository.java
@@ -2,14 +2,11 @@ package com.wsws.moduleinfra.repo.feed;
 
 import com.wsws.moduleinfra.entity.feed.AnswerEntity;
 import jakarta.persistence.LockModeType;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -31,28 +28,5 @@ public interface JpaAnswerRepository extends JpaRepository<AnswerEntity, Long> {
 
     @Query("SELECT COUNT(a) > 0 FROM AnswerEntity a WHERE a.userId = :userId AND a.questionEntity.id = :questionId")
     boolean existsByUserIdAndQuestionId(String userId, Long questionId);
-
-    /**
-     * 좋아요 수가 많은 인기답변 n개 (카테고리 O)
-     */
-    @Query("""
-            SELECT a
-            FROM AnswerEntity a join a.questionEntity q
-            WHERE q.categoryId = :categoryId
-            AND q.questionStatus = 'ACTIVATED'
-            ORDER BY a.likeCount DESC
-            """)
-    List<AnswerEntity> findAllByCategoryIdOrderByLikeCountDescWithCursor(Long categoryId, Pageable pageable);
-
-    /**
-     * 좋아요 수가 많은 인기답변 n개 (카테고리 X)
-     */
-    @Query("""
-            SELECT a
-            FROM AnswerEntity a join a.questionEntity q
-            WHERE q.questionStatus = 'ACTIVATED'
-            ORDER BY a.likeCount DESC
-            """)
-    List<AnswerEntity> findAllOrderByLikeCountDescWithCursor(Pageable pageable);
 
 }
